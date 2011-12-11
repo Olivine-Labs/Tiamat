f986eca6d1d38cf7387f6d841259a4e345ce37d0
Drew Ditthardt <dditthardt@olivinelabs.com>
2011-12-11 01:13:16 -0800

    Added newly generated assembly information.

---
+++ Router/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Router")]
+[assembly: AssemblyDescription("Tiamat Router Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Olivine Labs, LLC")]
+[assembly: AssemblyProduct("Tiamat")]
+[assembly: AssemblyCopyright("2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a61549bc-b799-4bf7-87b1-df8f4e4b6dea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
